bd3c0639ecacc92556fe293af559ad6627fce65a
Jia Jia <angersax@sina.com>
2021-02-02 19:01:34 +0800

    Refactor code

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ FILE=Dockerfile
 NAME=craftslab/gerritdocker
 TAG=3.3.2-ubuntu20
 
-pushd ubuntu/20
 docker build --no-cache -f $FILE -t $NAME:$TAG .
-popd
 
 #docker inspect $NAME:$TAG > Dockerobjects
